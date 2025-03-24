14365e30de07bc161f5acb799d5d394f3ed28114
wadill <ewomaodiri@gmail.com>
2025-03-24 10:42:50 +0100

    Upkeep compilation and deployment

--- pool-chainlink-auto/contracts/Pool.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.20;
-
-import "./common/Utils.sol";
-import "@chainlink/contracts/src/v0.8/automation/interfaces/AutomationCompatibleInterface.sol";
-import "@openzeppelin/contracts/utils/Pausable.sol";
